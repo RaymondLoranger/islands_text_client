7ed564f283025add25d92fdc4dcae88bb0e7e6b5
RaymondLoranger <Raymond.Loranger@bell.net>
2018-03-31 19:38:43 -0400

    0.1.1

--- config/persist_book_and_course_ref.exs
+++ config/persist_book_and_course_ref.exs
@@ -5,7 +5,7 @@ config :islands_text_client,
     """
     Inspired by the book [Functional Web Development]
     (https://pragprog.com/book/lhelph/functional-web-development-
-    with-elixir-otp-and-phoenix) by Lance Halvorsen
+    with-elixir-otp-and-phoenix) by Lance Halvorsen\s
     and the course [Elixir for Programmers]
     (https://codestool.coding-gnome.com/courses/
     elixir-for-programmers) by Dave Thomas.
