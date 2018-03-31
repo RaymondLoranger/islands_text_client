6ad466fa1ddd8a820faf1c9baa0992e077e9fd7a
RaymondLoranger <Raymond.Loranger@bell.net>
2018-03-31 19:29:55 -0400

    0.1.1

---
+++ config/persist_book_and_course_ref.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+config :islands_text_client,
+  book_and_course_ref:
+    """
+    Inspired by the book [Functional Web Development]
+    (https://pragprog.com/book/lhelph/functional-web-development-
+    with-elixir-otp-and-phoenix) by Lance Halvorsen
+    and the course [Elixir for Programmers]
+    (https://codestool.coding-gnome.com/courses/
+    elixir-for-programmers) by Dave Thomas.
+    """
+    |> String.replace("\n", "")
