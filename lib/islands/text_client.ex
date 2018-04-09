6c8ae34582e2c58a66e6ce200e764ba7441ecc85
RaymondLoranger <Raymond.Loranger@bell.net>
2018-04-09 00:04:37 -0400

    0.1.22

--- lib/islands/text_client.ex
+++ lib/islands/text_client.ex
@@ -18,16 +18,16 @@ defmodule Islands.TextClient do
   @doc """
   Lets player1 start a game.
 
-  App `:islands_engine` must run in node `:islands@<hostname>`...
+  App `islands_engine` must run in node with short name `islands`...
   - `cd islands_engine`
   - `iex --sname islands -S mix`
-  - `:observer.start()`
+  - `:observer.start() # optional`
 
-  Player1 must run in her node...
+  Player1 runs in a node with any short name (here `client1`)...
   - `cd islands_text_client`
-  - `iex --sname game<i>_player1 -S mix`
+  - `iex --sname client1 -S mix`
 
-  Player 1 starts a game from her node:
+  Player1 (here "Eve") starts a game from her node like so:
   - `Islands.TextClient.start("Eve")`
   """
   defdelegate start(game_name), to: Interact
@@ -35,11 +35,11 @@ defmodule Islands.TextClient do
   @doc """
   Lets player2 join a game.
 
-  Player2 must run in his node...
+  Player2 runs in a node with any short name (here `client2`)...
   - `cd islands_text_client`
-  - `iex --sname game<i>_player2 -S mix`
+  - `iex --sname client2 -S mix`
 
-  Player 2 joins a game from his node:
+  Player2 (here "Adam") joins a game from his node like so:
   - `Islands.TextClient.join("Eve", "Adam")`
   """
   defdelegate join(game_name, player_name), to: Interact
