8aa677d95be98b77f91a961ded42263b48977cc0
RaymondLoranger <Raymond.Loranger@bell.net>
2018-10-31 22:29:51 -0400

    0.1.43

--- lib/islands/text_client/interact.ex
+++ lib/islands/text_client/interact.ex
@@ -1,26 +1,11 @@
 defmodule Islands.TextClient.Interact do
-  @moduledoc """
-  Interacts with a client playing the Islands game.
-  """
-  # @moduledoc false
+  # @moduledoc """
+  # Interacts with a client playing the Islands game.
+  # """
+  @moduledoc false
 
   alias Islands.TextClient.{Player, RemoteProcedureCall, State}
 
-  @doc """
-  Lets player1 start a game.
-
-  App `islands_engine` must run in node with short name `islands`...
-  - `cd islands_engine`
-  - `iex --sname islands -S mix`
-  - `:observer.start() # optional`
-
-  Player1 runs in a node with any short name (here `client1`)...
-  - `cd islands_text_client`
-  - `iex --sname client1 -S mix`
-
-  Player1 (`Adam`) starts a game (`Eden`) from his node like so:
-  - `Islands.TextClient.start("Eden", "Adam")`
-  """
   @spec start(String.t(), String.t()) :: no_return
   def start(game_name, player_name)
       when is_binary(game_name) and is_binary(player_name) do
@@ -30,16 +15,6 @@ defmodule Islands.TextClient.Interact do
     |> Player.play()
   end
 
-  @doc """
-  Lets player2 join a game.
-
-  Player2 runs in a node with any short name (here `client2`)...
-  - `cd islands_text_client`
-  - `iex --sname client2 -S mix`
-
-  Player2 (`Eve`) joins game `Eden` from her node like so:
-  - `Islands.TextClient.join("Eden", "Eve")`
-  """
   @spec join(String.t(), String.t()) :: no_return
   def join(game_name, player_name)
       when is_binary(game_name) and is_binary(player_name) do
