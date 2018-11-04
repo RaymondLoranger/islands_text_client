7de90ed61b4a2eb487bbee7568cb644df1295926
RaymondLoranger <Raymond.Loranger@bell.net>
2018-11-04 17:00:38 -0500

    0.1.52

---
+++ lib/islands/text_client/summary/point.ex
@@ -0,0 +1,6 @@
+defmodule Islands.TextClient.Summary.Point do
+  alias Islands.Engine.Coord
+
+  @spec format(Coord.row(), Coord.col()) :: String.t()
+  def format(row, col), do: "(#{row}, #{col})"
+end
