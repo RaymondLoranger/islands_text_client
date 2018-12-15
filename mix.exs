ecc8a8977f0cea697be54c3e4c9c6837c71bf138
RaymondLoranger <Raymond.Loranger@bell.net>
2018-12-15 16:52:49 -0500

    0.1.83

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Text.Client.MixProject do
   def project do
     [
       app: :islands_text_client,
-      version: "0.1.82",
+      version: "0.1.83",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "Islands Text Client",
