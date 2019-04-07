add260454368c7a1ef9a11fa508fbccb91f8145d
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-06 20:08:54 -0400

    0.2.14

---
+++ config/_persist.prod.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :log_reset, reset?: true
+config :file_only_logger, log?: true
---
+++ config/_persist.test.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :log_reset, reset?: false
+config :file_only_logger, log?: false
