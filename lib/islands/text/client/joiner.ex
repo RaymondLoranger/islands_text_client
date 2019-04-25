f5dc5b5d6ba1b53be7ebfc693ef59343aba1d8a5
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-25 15:01:11 -0400

    0.2.27

--- lib/islands/text/client/joiner.ex
+++ lib/islands/text/client/joiner.ex
@@ -5,18 +5,15 @@ defmodule Islands.Text.Client.Joiner do
 
   import Islands.Text.Client.Guard
 
-  alias Islands.Text.Client.Options
   alias Islands.Client.{Player, RPC, State}
   alias Islands.Player, as: Gamer
 
   @spec join(String.t(), String.t(), Gamer.gender(), Keyword.t()) :: no_return
   def join(game_name, player_name, gender, options \\ [])
       when valid_game_args(game_name, player_name, gender, options) do
-    {mode, pause} = Options.get(options)
-
     game_name
     |> RPC.add_player(player_name, gender)
-    |> State.new(:player2, player_name, gender, mode, pause)
+    |> State.new(:player2, player_name, gender, options)
     |> Player.play()
   end
 end
